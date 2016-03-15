49e75b5c187a3780c7eb00193faf0a15d1634589
Eric des Courtis <eric.des.courtis@benbria.ca>
2016-03-15 14:22:17 -0400

    Add support for g722.

--- c_src/espandsp.c
+++ c_src/espandsp.c
@@ -5,9 +5,15 @@
 #include <arpa/inet.h>
 #include <spandsp.h>
 #include <spandsp/g711.h>
+#include <spandsp/private/g711.h>
+#include <spandsp/g722.h>
+#include <spandsp/private/g722.h>
 #include "erl_nif.h"
 
 static ErlNifResourceType *spandsp_g711_type = NULL;
+static ErlNifResourceType *spandsp_g722_encode_type = NULL;
+static ErlNifResourceType *spandsp_g722_decode_type = NULL;
+
 
 static ERL_NIF_TERM mk_atom(ErlNifEnv* env, const char* atom)
 {
@@ -140,12 +146,220 @@ static ERL_NIF_TERM g711_transcode_nif(ErlNifEnv* env, int argc, const ERL_NIF_T
     return enif_make_binary(env, &out_bin);
 }
 
+static ERL_NIF_TERM g722_encode_init_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+    g722_encode_state_t *state = NULL;
+    g722_encode_state_t *ctx;
+    ERL_NIF_TERM head;
+    ERL_NIF_TERM tail;
+    ERL_NIF_TERM list;
+    ERL_NIF_TERM resource;
+    int rate;
+    int options = 0;
+    unsigned len;
+    unsigned i;
+    char buf[64];
+
+    if(argc != 2) return enif_make_badarg(env);
+
+    if(!enif_get_int(env, argv[0], &rate)){
+        return enif_make_badarg(env);
+    }
+
+    list = argv[1];
+
+    if(!enif_get_list_length(env, list, &len)){
+        return enif_make_badarg(env);
+    }
+
+    for(i = 0; i < len; i++){
+        if(!enif_get_list_cell(env, list, &head, &tail)){
+            return enif_make_badarg(env);
+        }
+        list = tail;
+        if(!enif_get_atom(env, head, buf, (unsigned)sizeof(buf), ERL_NIF_LATIN1)){
+            return enif_make_badarg(env);
+        }
+
+        if(strcmp(buf, "sample_rate_8000") == 0){
+            options |= G722_SAMPLE_RATE_8000;
+        }else if(strcmp(buf, "packed") == 0){
+            options |= G722_PACKED;
+        }else{
+            return enif_make_badarg(env);
+        }
+    }
+
+    switch(rate){
+    case 64000:
+    case 56000:
+    case 48000:
+        break;
+    default:
+        return mk_error(env, "unsupported_bit_rate");
+    }
+
+    state = (g722_encode_state_t *)enif_alloc_resource(
+        spandsp_g722_encode_type,
+        sizeof(g722_encode_state_t)
+    );
+
+    ctx = g722_encode_init(state, rate, options);
+    if(ctx == NULL){
+        enif_release_resource(state);
+        return mk_error(env, "g722_encode_init");
+    }
+
+    resource = enif_make_resource(env, state);
+    enif_release_resource(state);
+    return resource;
+}
+
+static ERL_NIF_TERM g722_encode_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    size_t len;
+    g722_encode_state_t *state = NULL;
+    ErlNifBinary bin;
+    ErlNifBinary out_bin;
+
+    if(argc != 2) return enif_make_badarg(env);
+
+    if(!enif_get_resource(env, argv[0], spandsp_g722_encode_type, (void **)&state))
+        return enif_make_badarg(env);
+
+    assert(state != NULL);
+
+    if(!enif_inspect_binary(env, argv[1], &bin))
+        return enif_make_badarg(env);
+
+    len = bin.size / sizeof(int16_t);
+    if(len > (size_t)INT_MAX) return enif_make_badarg(env);
+
+    if(!enif_alloc_binary(len, &out_bin)) return enif_make_badarg(env);
+
+    (void)g722_encode(state, (uint8_t *)out_bin.data, (int16_t *)bin.data, (int)len);
+
+    return enif_make_binary(env, &out_bin);
+}
+
+static ERL_NIF_TERM g722_decode_init_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+    g722_decode_state_t *state = NULL;
+    g722_decode_state_t *ctx;
+    ERL_NIF_TERM head;
+    ERL_NIF_TERM tail;
+    ERL_NIF_TERM list;
+    ERL_NIF_TERM resource;
+    int rate;
+    int options = 0;
+    unsigned len;
+    unsigned i;
+    char buf[64];
+
+    if(argc != 2) return enif_make_badarg(env);
+
+    if(!enif_get_int(env, argv[0], &rate)){
+        return enif_make_badarg(env);
+    }
+
+    list = argv[1];
+
+    if(!enif_get_list_length(env, list, &len)){
+        return enif_make_badarg(env);
+    }
+
+    for(i = 0; i < len; i++){
+        if(!enif_get_list_cell(env, list, &head, &tail)){
+            return enif_make_badarg(env);
+        }
+        list = tail;
+        if(!enif_get_atom(env, head, buf, (unsigned)sizeof(buf), ERL_NIF_LATIN1)){
+            return enif_make_badarg(env);
+        }
+
+        if(strcmp(buf, "sample_rate_8000") == 0){
+            options |= G722_SAMPLE_RATE_8000;
+        }else if(strcmp(buf, "packed") == 0){
+            options |= G722_PACKED;
+        }else{
+            return enif_make_badarg(env);
+        }
+    }
+
+    switch(rate){
+    case 64000:
+    case 56000:
+    case 48000:
+        break;
+    default:
+        return mk_error(env, "unsupported_bit_rate");
+    }
+
+    state = (g722_decode_state_t *)enif_alloc_resource(
+        spandsp_g722_decode_type,
+        sizeof(g722_decode_state_t)
+    );
+
+    ctx = g722_decode_init(state, rate, options);
+    if(ctx == NULL){
+        enif_release_resource(state);
+        return mk_error(env, "g722_decode_init");
+    }
+
+    resource = enif_make_resource(env, state);
+    enif_release_resource(state);
+    return resource;
+}
+
+static ERL_NIF_TERM g722_decode_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    size_t len;
+    g722_decode_state_t *state = NULL;
+    ErlNifBinary bin;
+    ErlNifBinary out_bin;
+
+    if(argc != 2) return enif_make_badarg(env);
+
+    if(!enif_get_resource(env, argv[0], spandsp_g722_decode_type, (void **)&state))
+        return enif_make_badarg(env);
+
+    assert(state != NULL);
+
+    if(!enif_inspect_binary(env, argv[1], &bin))
+        return enif_make_badarg(env);
+
+    len = bin.size * sizeof(int16_t);
+    if(len > (size_t)INT_MAX) return enif_make_badarg(env);
+
+    if(!enif_alloc_binary(len, &out_bin)) return enif_make_badarg(env);
+
+    (void)g722_decode(state, (int16_t *)out_bin.data, (uint8_t *)bin.data, (int)bin.size);
+    return enif_make_binary(env, &out_bin);
+}
+
+
 static void garbage_collect_spandsp_g711(ErlNifEnv *env, void *p)
 {
     g711_state_t *state;
 
     state = (g711_state_t *)p;
     g711_release(state);
+}
+
+static void garbage_collect_spandsp_g722_encode(ErlNifEnv *env, void *p)
+{
+    g722_encode_state_t *state;
+
+    state = (g722_encode_state_t *)p;
+    g722_encode_release(state);
+}
+
+static void garbage_collect_spandsp_g722_decode(ErlNifEnv *env, void *p)
+{
+    g722_decode_state_t *state;
+
+    state = (g722_decode_state_t *)p;
+    g722_decode_release(state);
 }
 
 static int handle_load(ErlNifEnv *env, void **priv, ERL_NIF_TERM load_info)
@@ -166,6 +380,32 @@ static int handle_load(ErlNifEnv *env, void **priv, ERL_NIF_TERM load_info)
 
     spandsp_g711_type = rt;
 
+    rt = enif_open_resource_type(
+        env,
+        NULL,
+        "spandsp_g722_encode_type",
+        garbage_collect_spandsp_g722_encode,
+        ERL_NIF_RT_CREATE,
+        &rf
+    );
+
+    if(rt == NULL) return -1;
+
+    spandsp_g722_encode_type = rt;
+
+    rt = enif_open_resource_type(
+        env,
+        NULL,
+        "spandsp_g722_decode_type",
+        garbage_collect_spandsp_g722_decode,
+        ERL_NIF_RT_CREATE,
+        &rf
+    );
+
+    if(rt == NULL) return -1;
+
+    spandsp_g722_decode_type = rt;
+
     return 0;
 }
 
@@ -175,11 +415,15 @@ static int handle_upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data
 }
 
 static ErlNifFunc nif_funcs[] = {
-            {"output_encoding_nif", 0, output_encoding_nif, 0},
-            {"g711_open_nif",       1, g711_open_nif,       0},
-            {"g711_encode_nif",     2, g711_encode_nif,     0},
-            {"g711_decode_nif",     2, g711_decode_nif,     0},
-            {"g711_transcode_nif",  2, g711_transcode_nif,  0}
+            {"output_encoding_nif",     0, output_encoding_nif,     0},
+            {"g711_open_nif",           1, g711_open_nif,           0},
+            {"g711_encode_nif",         2, g711_encode_nif,         0},
+            {"g711_decode_nif",         2, g711_decode_nif,         0},
+            {"g711_transcode_nif",      2, g711_transcode_nif,      0},
+            {"g722_encode_init_nif",    2, g722_encode_init_nif,    0},
+            {"g722_encode_nif",         2, g722_encode_nif,         0},
+            {"g722_decode_init_nif",    2, g722_decode_init_nif,    0},
+            {"g722_decode_nif",         2, g722_decode_nif,         0}
 };
 
 ERL_NIF_INIT(espandsp, nif_funcs, handle_load, NULL, handle_upgrade, NULL);
--- src/espandsp.erl
+++ src/espandsp.erl
@@ -6,7 +6,11 @@
     g711_open/1,
     g711_encode/2,
     g711_decode/2,
-    g711_transcode/2
+    g711_transcode/2,
+    g722_encode_init/2,
+    g722_encode/2,
+    g722_decode_init/2,
+    g722_decode/2
 ]).
 -on_load(init/0).
 
@@ -17,6 +21,12 @@
 -type g711_alaw()  :: binary().
 -type g711()       :: g711_ulaw() | g711_alaw().
 
+-opaque g722_encode_ctx() :: binary().
+-opaque g722_decode_ctx() :: binary().
+-type g722()            :: binary().
+-type g722_rate()       :: 64000 | 56000 | 48000.
+-type g722_option()     :: sample_rate_8000 | packed.
+
 -type pcm_s16le()  :: binary().
 -type pcm_s16be()  :: binary().
 -type pcm_s16()    :: pcm_s16le() | pcm_s16be().
@@ -26,6 +36,13 @@
     g711_ulaw/0,
     g711_alaw/0,
     g711/0
+]).
+
+-export_type([
+    g722_encode_ctx/0,
+    g722_decode_ctx/0,
+    g722/0,
+    g722_rate/0
 ]).
 
 -export_type([
@@ -58,6 +75,28 @@ g711_decode(Ref, Bin) when is_binary(Bin), byte_size(Bin) =< ?MAX_SIZE ->
 g711_transcode(Ref, Bin) when is_binary(Bin), byte_size(Bin) =< ?MAX_SIZE ->
     g711_transcode_nif(Ref, Bin).
 
+-spec(g722_encode_init(g722_rate(), [g722_option()]) -> g722_encode_ctx()).
+g722_encode_init(Rate, Options)
+    when is_integer(Rate),
+    (Rate =:= 64000) or (Rate =:= 56000) or (Rate =:= 48000),
+    is_list(Options) ->
+    g722_encode_init_nif(Rate, Options).
+
+-spec(g722_encode(g722_encode_ctx(), pcm_s16()) -> g722()).
+g722_encode(Ref, Bin) when is_binary(Bin), byte_size(Bin) =< ?MAX_SIZE ->
+    g722_encode_nif(Ref, Bin).
+
+-spec(g722_decode_init(g722_rate(), [g722_option()]) -> g722_decode_ctx()).
+g722_decode_init(Rate, Options)
+    when is_integer(Rate),
+    (Rate =:= 64000) or (Rate =:= 56000) or (Rate =:= 48000),
+    is_list(Options) ->
+    g722_decode_init_nif(Rate, Options).
+
+-spec(g722_decode(g722_decode_ctx(), g722()) -> pcm_s16()).
+g722_decode(Ref, Bin) when is_binary(Bin), byte_size(Bin) =< ?MAX_SIZE ->
+    g722_decode_nif(Ref, Bin).
+
 g711_open_nif(_) ->
     not_loaded(?LINE).
 
@@ -68,6 +107,18 @@ g711_decode_nif(_, _) ->
     not_loaded(?LINE).
 
 g711_transcode_nif(_, _) ->
+    not_loaded(?LINE).
+
+g722_encode_init_nif(_, _) ->
+    not_loaded(?LINE).
+
+g722_encode_nif(_, _) ->
+    not_loaded(?LINE).
+
+g722_decode_init_nif(_, _) ->
+    not_loaded(?LINE).
+
+g722_decode_nif(_, _) ->
     not_loaded(?LINE).
 
 -spec(output_encoding() -> pcm_s16le | pcm_s16be).
